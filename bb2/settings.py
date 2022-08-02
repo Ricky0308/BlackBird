ac7d5ef2431a86af4a265773aec4b893b3592a66
門田 凌典 <Kadotaryosuke@monta-no-MacBook-Air.local>
2022-08-02 13:45:31 +0900

    practice

--- bb2/settings.py
+++ bb2/settings.py
@@ -13,6 +13,8 @@ from telnetlib import AUTHENTICATION
 import environ
 from pathlib import Path
 import os
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kh(e)%3ywxod5enfz1c+70o%4q48odo$phz-nvdyi64*e=!v3s'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -148,8 +150,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Email  
-env = environ.Env()
-environ.Env.read_env()
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
